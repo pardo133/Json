388fd4ecb6bcdbe0567e877a2623b3457318d0af
pardo133 <pardo133@hotmail.com>
2026-03-24 10:23:38 +0100

    modificacion

---
+++ script.js
@@ -0,0 +1,23 @@
+const usuariosVIP = [
+    {
+        username: "capitanSalami",
+        puntosAcumulados: 1500,
+        suscripcionActiva: true,
+        juegosFavoritos: ["Zelda", "Mario Kart", "Elder Ring"]
+    },
+    {
+        username: "noNAME34",
+        puntosAcumulados: 850,
+        suscripcionActiva: false,
+        juegosFavoritos: ["FIFA", "Call of Duty"]
+    },
+    {
+        username: "PARDO133",
+        puntosAcumulados: 2100,
+        suscripcionActiva: true,
+        juegosFavoritos: ["Call of Dutty", "Assasins Creed", "Fortnite"]
+    }
+];
+
+
+console.log(usuariosVIP);
