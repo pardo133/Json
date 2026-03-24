bc81078c60afb17971329772430dbb63e2aefd1d
pardo133 <pardo133@hotmail.com>
2026-03-24 12:10:26 +0100

    f

--- script.js
+++ script.js
@@ -1,46 +1,23 @@
-const inventarioTienda = [
-  {
-    titulo: "Fifa",
-    genero: ["deporte", "multijugador"],
-    precio: "60",
-    esDigital: "true",
-  },
-
-  {
-    titulo: "GTA",
-    esDigital: "true",
-    genero: ["accion", "online"],
-    precio: "40",
-    esDigital: "false",
-  },
-  {
-    titulo: "Tekken",
-    precio: "20",
-    genero: ["lucha", "facil"],
-    esDigital: "false",
-  },
-];
-console.log(inventarioTienda);
-
 const usuariosVIP = [
-  {
-    username: "capitanSalami",
-    puntosAcumulados: 1500,
-    suscripcionActiva: true,
-    juegosFavoritos: ["Zelda", "Mario Kart", "Elder Ring"],
-  },
-  {
-    username: "noNAME34",
-    puntosAcumulados: 850,
-    suscripcionActiva: false,
-    juegosFavoritos: ["FIFA", "Call of Duty"],
-  },
-  {
-    username: "PARDO133",
-    puntosAcumulados: 2100,
-    suscripcionActiva: true,
-    juegosFavoritos: ["Call of Dutty", "Assasins Creed", "Fortnite"],
-  },
+    {
+        username: "capitanSalami",
+        puntosAcumulados: 1500,
+        suscripcionActiva: true,
+        juegosFavoritos: ["Zelda", "Mario Kart", "Elder Ring"]
+    },
+    {
+        username: "noNAME34",
+        puntosAcumulados: 850,
+        suscripcionActiva: false,
+        juegosFavoritos: ["FIFA", "Call of Duty"]
+    },
+    {
+        username: "PARDO133",
+        puntosAcumulados: 2100,
+        suscripcionActiva: true,
+        juegosFavoritos: ["Call of Dutty", "Assasins Creed", "Fortnite"]
+    }
 ];
+
 
 console.log(usuariosVIP);
